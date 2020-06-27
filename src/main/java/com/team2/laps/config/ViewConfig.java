f8abcbb353f150680c78f6e022d17fda428850f5
Martini024 <zhao435021640@gmail.com>
2020-06-27 22:55:42 +0800

    config date format inside json response

--- src/main/java/com/team2/laps/config/ViewConfig.java
+++ src/main/java/com/team2/laps/config/ViewConfig.java
@@ -1,6 +1,10 @@
 package com.team2.laps.config;
 
+import java.text.SimpleDateFormat;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +17,12 @@ public class ViewConfig implements WebMvcConfigurer {
         registry.addViewController("/view/dashboard").setViewName("/dashboard/home");
         registry.addViewController("/view/staff/leave").setViewName("/dashboard/staff/leave");
         registry.addViewController("/view/staff/compensation").setViewName("/dashboard/staff/compensation");
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
+        jackson2ObjectMapperBuilder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy/MM/dd-HH:mm"));
+        return jackson2ObjectMapperBuilder;
     }
 }
