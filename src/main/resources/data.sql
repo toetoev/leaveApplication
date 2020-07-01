3a673d816f603be19a80f14503087ebf9b1d2c36
Martini024 <zhao435021640@gmail.com>
2020-07-01 14:23:59 +0800

    auto-populate rolename and admin account

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,13 @@
 --  Auto-generated SQL script #202006251623
-INSERT INTO Java.roles (name)
+INSERT IGNORE INTO roles (name)
 	VALUES ('ROLE_ADMINISTRATIVE_STAFF');
-INSERT INTO Java.roles (name)
+INSERT IGNORE INTO roles (name)
 	VALUES ('ROLE_PROFESSIONAL_STAFF');
-INSERT INTO Java.roles (name)
+INSERT IGNORE INTO roles (name)
 	VALUES ('ROLE_ADMIN');
-INSERT INTO Java.roles (name)
+INSERT IGNORE INTO roles (name)
 	VALUES ('ROLE_MANAGER');
+
+SET @UUID = (SELECT UUID() AS UUID);
+INSERT IGNORE INTO users (id, name, username, password) VALUES (@UUID, 'Admin','admin','$2a$10$HmwZebguqxttEraUTFvwpuErunkGhk.5oFSVnRnl0esn3tpy.TXBG');
+INSERT IGNORE INTO user_roles (user_id, role_id) VALUES (@UUID,'ROLE_ADMIN');
