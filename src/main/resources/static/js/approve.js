f57cecbb4a955020b5f79ce2d289ea5f1ab5de49
Martini024 <zhao435021640@gmail.com>
2020-07-03 20:44:58 +0800

    clean some english bug

--- src/main/resources/static/js/approve.js
+++ src/main/resources/static/js/approve.js
@@ -1,53 +1,4 @@
 var dataTable;
-function edit_action(this_el, item_id) {
-	$("#item_id").val(item_id);
-	var tr_el = this_el.closest("tr");
-	var row = dataTable.row(tr_el);
-	var row_data = row.data();
-	$.fn.datetimepicker.Constructor.Default = $.extend(
-		{},
-		$.fn.datetimepicker.Constructor.Default,
-		{
-			icons: {
-				time: "fa fa-clock",
-				date: "fa fa-calendar",
-				up: "fa fa-arrow-up",
-				down: "fa fa-arrow-down",
-				previous: "fa fa-chevron-left",
-				next: "fa fa-chevron-right",
-				today: "fa fa-calendar-check-o",
-				clear: "fa fa-trash",
-				close: "fa fa-times",
-			},
-			format: "MM/DD/YYYY HH",
-			enabledHours: [8, 13],
-			locale: "sg",
-		}
-	);
-
-	$("#startDatetimepicker").datetimepicker({
-		daysOfWeekDisabled: [0, 6],
-		minDate: moment(row_data.startDate, "MM/DD/YYYY HH"),
-		defaultDate: moment(row_data.startDate, "MM/DD/YYYY HH"),
-		format: "MM/DD/YYYY HH",
-	});
-	$("#endDatetimepicker").datetimepicker({
-		daysOfWeekDisabled: [0, 6],
-		defaultDate: moment(row_data.endDate, "MM/DD/YYYY HH"),
-		format: "MM/DD/YYYY HH",
-	});
-	$("#startDatetimepicker").on("change.datetimepicker", function (e) {
-		$("#endDatetimepicker").datetimepicker("minDate", e.date);
-	});
-	$("#endDatetimepicker").on("change.datetimepicker", function (e) {
-		$("#startDatetimepicker").datetimepicker("maxDate", e.date);
-	});
-
-	$("#leave-type").val(row_data.leaveType);
-	$("#reason").val(row_data.reason);
-	$("#work-dissemination").val(row_data.workDissemination);
-	$("#contact-details").val(row_data.contactDetails);
-}
 
 function approve_action(this_el) {
 	var tr_el = this_el.closest("tr");
@@ -179,36 +130,4 @@ $(document).ready(function () {
 	if (localStorage.getItem("accessToken")) {
 		dataTable = initDataTable();
 	}
-});
-
-$("#saveEdit").click(function (e) {
-	e.preventDefault();
-	$.ajax({
-		type: "POST",
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		url: "/api/leaves",
-		headers: {
-			Authorization: "Bearer " + localStorage.getItem("accessToken"),
-		},
-		data: JSON.stringify({
-			id: $("#item_id").val(),
-			startDate: $("#startDatetimepicker")
-				.datetimepicker("date")
-				.format("MM/DD/YYYY HH"),
-			endDate: $("#endDatetimepicker")
-				.datetimepicker("date")
-				.format("MM/DD/YYYY HH"),
-			leaveType: $("#leave-type").val(),
-			reason: $("#reason").val(),
-			workDissemination: $("#work-dissemination").val(),
-			contactDetails: $("#contact-details").val(),
-			status: "UPDATED",
-		}),
-		success: function (res) {
-			console.log(res);
-			dataTable.ajax.reload();
-			$("#editModal").modal("hide");
-		},
-	});
 });
