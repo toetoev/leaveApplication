97674e897a8b90f4e1d956784481417f47298f40
Martini024 <zhao435021640@gmail.com>
2020-07-03 20:46:57 +0800

    dashboard redirect to auth when not logged in

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -3,3 +3,8 @@ $("#signOut").click(function (e) {
 	localStorage.removeItem("accessToken");
 	window.location = "/view/auth";
 });
+
+$(document).ready(function () {
+	if (localStorage.getItem("accessToken") == null)
+		window.location = "/view/auth";
+});
