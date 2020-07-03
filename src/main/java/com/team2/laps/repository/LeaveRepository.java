5ba655d3be9a71ac5faab28102c4ecc4ef26b77f
Martini024 <zhao435021640@gmail.com>
2020-07-03 20:44:14 +0800

    fix count current year leave used

--- src/main/java/com/team2/laps/repository/LeaveRepository.java
+++ src/main/java/com/team2/laps/repository/LeaveRepository.java
@@ -3,7 +3,6 @@ package com.team2.laps.repository;
 import java.util.List;
 
 import com.team2.laps.model.Leave;
-import com.team2.laps.model.LeaveType;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,5 +18,5 @@ public interface LeaveRepository extends JpaRepository<Leave, String> {
     List<Leave> findLeaveForApprovalBySubordinatesOrderByStartDate(@Param("id") String id);
 
     @Query(value = "SELECT COALESCE(SUM(DATEDIFF(end_date, start_date)), 0) FROM leaves WHERE leaves.user_id = :id AND leave_type = :leave_type AND status = '4' AND YEAR(start_date) = YEAR(CURDATE()) AND YEAR(end_date) = YEAR(CURDATE())", nativeQuery = true)
-    int countCurrentYearLeaveUsed(@Param("id") String id, @Param("leave_type") LeaveType leaveType);
+    int countCurrentYearLeaveUsed(@Param("id") String id, @Param("leave_type") int leaveType);
 }
